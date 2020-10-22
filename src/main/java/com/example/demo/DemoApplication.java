dd60fda0be2e2b22a06e77cc59dfaee512aa6f5d
Julius Schinschke <mail@julius-schinschke.de>
2020-10-22 21:27:00 +0200

    testQualityGate

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,16 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 
-    public     String password = "superSecret";
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    public static int  add(int a, int b){
-        return a+b;
-    }
 
-    public static int addInt(int a, int b) {
-        return a+b;
-    }
 }
